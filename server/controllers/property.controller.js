892368614677e7472890891900a01f8d93b828a3
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-02-18 17:33:51 +0100

    Backend getAllproperties middleware added

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -5,8 +5,6 @@ import mongoose from 'mongoose'
 import Property from '../mongoDB/models/property.js'
 import User from '../mongoDB/models/user.js'
 
-
-
 dotenv.config()
 
 cloudinary.config({
@@ -15,10 +13,34 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
+/* getAllProperties  permet de récupérer une liste de propriétés en fonction des critères de recherche spécifiés dans les paramètres de requête
+ de l'URL. Elle gère également la pagination et le tri des résultats, et fournit des en-têtes HTTP pour permettre une utilisation appropriée de l'API.*/
+
 const getAllProperties = async (req, res) => {
 
+//Extrait les propriétés suivantes de l'objet req.query (qui contient les paramètres de requête de l'URL)
+//Si une valeur n'est pas présente dans les paramètres de requête, la variable locale correspondante sera initialisée à une chaîne vide
+  const { _end, _order, _start, _sort, title_like = '', propertyType = ''} = req.query 
+
+  const query = {}
+
+  if(propertyType !== '') {
+    query.propertyType = propertyType
+  }
+  if(title_like) {
+    query.title = { $regex: title_like, $option: 'i'}
+  }
   try {
-    const properties = await Property.find({}).limit(req.query._end)
+    const count = await Property.countDocuments({ query })
+    const properties = await Property
+    .find(query)
+    .limit(_end)
+    .skip(_start)
+    .sort({ [_sort]: _order})
+
+/*  Configuration des en-têtes HTTP de la réponse pour inclure le nombre total de propriétés dans l'en-tête x-total-count */
+    res.header('x-total-count', count)
+    res.header('Access-Control-Expose-Headers', 'x-total-count')
     res.status(200).json(properties)
   } catch (error) {
     res.status(500).json({ message: error.message })
@@ -33,11 +55,11 @@ const createProperty = async (req, res) => {
         title, description, propertyType, location, price, photo, email,
       } = req.body;
   
-      // Start a new session
+      // Démarer une nouvelle session
       const session = await mongoose.startSession();
       session.startTransaction();
   
-      // Retrieve user by email
+      // Trouver un user par son email
       const user = await User.findOne({ email }).session(session);
       if (!user) {
         throw new Error('User not found');
@@ -45,7 +67,7 @@ const createProperty = async (req, res) => {
   
       const photoUrl = await cloudinary.uploader.upload(photo);
   
-      // Create a new property
+      // Créer une nouvelle propriété
       const newProperty = await Property.create(
         {
           title,
@@ -58,14 +80,14 @@ const createProperty = async (req, res) => {
         },
       );
   
-      // Update the user's allProperties field with the new property
+      // Mettre à jour le champ allProperties de l'utilisateur avec la nouvelle propriété
       user.allProperties.push(newProperty._id);
       await user.save({ session });
   
-      // Commit the transaction
+      // Valider la transaction
       await session.commitTransaction();
   
-      // Send response
+      // Envoyer la réponse
       res.status(200).json({ message: 'Property created successfully' });
     } catch (err) {
       res.status(500).json({ message: 'Failed to create property, please try again later' });
