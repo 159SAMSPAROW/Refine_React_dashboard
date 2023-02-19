2c470ee39cf721c215bef3ae8f5ebbe88afdb9ba
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-02-19 17:57:51 +0100

    updateProperty function added

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -103,7 +103,28 @@ const createProperty = async (req, res) => {
     }
   };
 
-const updateProperty = async (req, res) => {}
+const updateProperty = async (req, res) => {
+  try{
+    const { id } = req.params
+    const { title, description, propertyType, location, price, photo } = req.body
+    let photoUrl = '';
+    if (photo) {
+      photoUrl = await cloudinary.uploader.upload(photo);
+    }
+
+    await Property.findByIdAndUpdate({ _id: id}, {
+      title,
+      description,
+      propertyType,
+      location,
+      price,
+      photo: photoUrl.url || photo
+    })
+    res.status(200).json({ message: 'Property updated successfully'})
+  } catch {
+    res.status(500).json({ message: error.message})
+  }
+}
 
 const deleteProperty = async (req, res) => {
   try{
@@ -114,7 +135,7 @@ const deleteProperty = async (req, res) => {
     
     const session = await mongoose.startSession()
     session.startTransaction()
-    
+
     propertyToDelete.remove({session})
     propertyToDelete.creator.allProperties.pull(propertyToDelete)
 
