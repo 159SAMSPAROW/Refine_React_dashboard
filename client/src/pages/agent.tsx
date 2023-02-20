09ff873b5acad7d23ba1dad049dcc31cba1bfda3
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-02-20 16:22:05 +0100

    AgentCard component added

--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import { useList } from '@pankod/refine-core'
+import { Box, Typography } from '@pankod/refine-mui'
+import { AgentCard } from 'components'
 
-const Agent = () => {
+const Agents = () => {
+
+  const { data, isLoading, isError } = useList({
+    resource: 'users',
+  })
+  const allAgents = data?.data ?? []
+  console.log(allAgents)
+  if(isLoading) return <div>loading...</div>
+  if(isError) return <div>Error</div>
+
+
   return (
-    <div>
-      Agent
-    </div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color='#11142d'>Agents List</Typography>
+      <Box mt='20px' sx={{display:'flex', flexWrap:'wrap', gap:'20px', backgroundColor:'#fcfcfc' }}>
+        {allAgents.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
   );
 }
 
-export default Agent;
+export default Agents;
--- client/src/pages/editProperty.tsx
+++ client/src/pages/editProperty.tsx
@@ -27,7 +27,7 @@ const CreateProperty = () => {
 /* onFinishHandler =  Si une image a été sélectionnée, la fonction soumet les données à la fonction onFinish avec l'URL de l'image
  et l'e-mail de l'utilisateur.*/
   const onFinishHandler = async (data: FieldValues) => {
-    //if(!propertyImage.name) return alert('Please upload a property image')
+    if(!propertyImage.name) return alert('Please upload a property image')
     await onFinish({ ...data, photo: propertyImage.url, email: user.email})
   }
 
