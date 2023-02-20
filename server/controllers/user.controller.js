728ad86512fa6c5235b5ec888f4882c329ff21bc
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-02-20 17:15:50 +0100

    My profile component added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -27,6 +27,16 @@ const createUser = async (req, res) => {
 
    
 }
-const getUserInfoByID = async (req, res) => {}
+const getUserInfoByID = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const userProperties = await User.findOne({ _id: id }).populate('allProperties');
+  
+      if (userProperties) res.status(200).json(userProperties);
+      else res.status(404).send('User not found');
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to get user properties, please try again later' });
+    }
+  };
 
 export { getAllUsers, createUser, getUserInfoByID}
